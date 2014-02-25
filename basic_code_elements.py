01595df9e6d1ee0aefe2ae154a400672f92c116c
edwardwas <ed@wastell.co.uk>
2014-02-25 15:13:38 +0000

    Change show_pic to save_pic, and create show_pic. Comments should explain why. Added imports to basic_code_elements.py so that the methods can be used

--- basic_code_elements.py
+++ basic_code_elements.py
@@ -4,7 +4,29 @@
 
 ## Uses axes, imshow, colorbar, title, savefig, close, figure
 ## from matplotlib.pyplot
-def show_pic(image, plot_name, colourmap=None):
+from matplotlib.pyplot import axes,imshow,colorbar,title,savefig,close,figure
+def show_pic(image):
+    """Method to simply shwo the image. Useful for tests"""
+    figure(facecolor='white', figsize=(5,4))
+
+    # Remove axes from the plot
+    ax = axes(frameon=False)
+    ax.get_yaxis().set_visible(False)
+    ax.get_xaxis().set_visible(False)
+
+    # Give the image a title
+    title(plot_name)
+    
+    if colourmap != None:
+        # Make image with a colourmap
+        imshow(image, interpolation='none',cmap=colourmap)
+        colorbar()
+    else:
+        # Make an image without a colourmap
+        imshow(image, interpolation='none')
+
+
+def save_pic(image, plot_name, colourmap=None):
     """A function that nicley abstracts producing an image object"""
     figure(facecolor='white', figsize=(5,4))
 
