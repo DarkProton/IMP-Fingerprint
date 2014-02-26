bd4107a0d40a8b0aa16bd65cd0fbe3ca44c109b1
edwardwas <ed@wastell.co.uk>
2014-02-26 21:31:15 +0000

    Did I branch correctly?

--- basic_code_elements.py
+++ basic_code_elements.py
@@ -83,6 +83,7 @@ def pnd(image):
         for m in range(1, x_pix):
             n1 = (-image[n, m] + image[n-1, m] + image[n-1, m-1] \
                   - image[n, m-1])/4
+            print (n1)
             n2 = (-image[n, m] - image[n-1, m] + image[n-1, m-1] \
                   + image[n, m-1])/4
 
